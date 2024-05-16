d4927b4b0489896b4398693ce4ef83b16ec02c0e
narendrab15 <42624134+narendrab15@users.noreply.github.com>
2024-05-16 10:59:56 -0700

    new
    removed file and added new files

---
+++ javaAlgorithms/src/main/java/com/javaApp/javaAlgorithms/JavaAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.javaApp.javaAlgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ javaAlgorithms/src/test/java/com/javaApp/javaAlgorithms/JavaAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaApp.javaAlgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
